439838541bab019f8682d679ced550959d7c0934
BE Sketens <maxime.kestens@hotmail.be>
2022-03-18 15:17:39 +0100

    v 1.0

---
+++ controller/quizList.inc.php
@@ -0,0 +1,44 @@
+<?php
+
+$list = [
+    0 => [
+        "Titre" => "",
+        "Illustration" => "",
+        "Auteur" => "",
+        "Questions" => [
+
+        ]
+    ],
+    1 => [
+        "Titre" => "",
+        "Illustration" => "",
+        "Auteur" => "",
+        "Questions" => [
+
+        ]
+    ],
+    2 => [
+        "Titre" => "",
+        "Illustration" => "",
+        "Auteur" => "",
+        "Questions" => [
+
+        ]
+    ],
+    3 => [
+        "Titre" => "",
+        "Illustration" => "",
+        "Auteur" => "",
+        "Questions" => [
+
+        ]
+    ],
+    4 => [
+        "Titre" => "",
+        "Illustration" => "",
+        "Auteur" => "",
+        "Questions" => [
+
+        ]
+    ],
+];
